a4c8012afa470c4d1b297d8e6772d2afbaf11f55
shobhit-u <shobhitumrao5@gmail.com>
2023-01-18 02:04:12 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import classes from "./App.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
-  return <div>Youtube</div>;
+  return (
+    <header className={classes.header}>
+      <section className={classes.logoGroup}>
+        <MenuIcon />
+        <YouTubeIcon />
+        Youtube
+      </section>
+      <section className={classes.searchGroup}>
+        <input type="text" />
+        <button>
+          <SearchIcon />
+        </button>
+      </section>
+      <section className={classes.buttonGrourp}>Buttons</section>
+    </header>
+  );
 }
 
 export default App;
